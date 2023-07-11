c16e5d713a0453a7d2d0c7ea56cc781f6a9bce00
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-11 14:22:09 +0300

    useEffect to fetch data from dbjson

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,7 +4,12 @@ import Sidebar from './SideBar';
 const MovieList = () => {
     const [movies, setMovies] = useState([]);
 
- 
+    useEffect(() => {
+        fetch('http://localhost:3000/movies')
+            .then(response => response.json())
+            .then(data => setMovies(data))
+            .catch(error => console.log(error));
+    }, []);
 
     return (
 

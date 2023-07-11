6b056380ae2f8900cce707d330d89abc841648f7
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-11 14:22:42 +0300

    render movie list

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -12,7 +12,28 @@ const MovieList = () => {
     }, []);
 
     return (
-
+        <div className='Movie-Flex'>
+            <div className='Sidebar-Flex'>
+                <Sidebar />
+            </div>
+            <div class="movie-container">
+                {movies.map(movie => (
+                    <div class="movie" key={movie.id}>
+                        <div className='movie-flex-wrow'>
+                            <div className='image-text-desc'>
+                                <img src={movie.image} className='movie-img' />
+                                <h2 class="movie-title">{movie.title}</h2>
+                            </div>
+                            <button className='btn-primary' type='submit'>Follow</button>
+                        </div>
+                        <p class="movie-info">Genre: {movie.genre}</p>
+                        <video class="movie-video" controls>
+                            <source src={process.env.PUBLIC_URL + movie.movieFile} type="video/mp4" />
+                        </video>
+                    </div>
+                ))}
+            </div>
+        </div>
 
     );
 };

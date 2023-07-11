384cfd40d7c5859c8acacc922772916ae5ae23bf
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-11 14:21:43 +0300

    MovieList component

---
+++ src/components/MovieList.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react';
+import './MovieList.css'
+import Sidebar from './SideBar';
+const MovieList = () => {
+    const [movies, setMovies] = useState([]);
+
+ 
+
+    return (
+
+
+    );
+};
+
+export default MovieList;
+
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -4,7 +4,24 @@ import './Sidebar.css'
 function Sidebar() {
     return (
         <div>
-
+            <div class="sidebar">
+                <div class="sidebar-item">
+                    <i class="fas fa-hashtag"></i>
+                    <span>Following</span>
+                </div>
+                <div class="sidebar-item">
+                    <i class="fas fa-music"></i>
+                    <span>Music</span>
+                </div>
+                <div class="sidebar-item">
+                    <i class="fas fa-users"></i>
+                    <span>Friends</span>
+                </div>
+                <div class="sidebar-item">
+                    <i class="fas fa-shopping-bag"></i>
+                    <span>Shop</span>
+                </div>
+            </div>
 
 
         </div>
